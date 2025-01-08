2e4a74a7a0c4ce8cffe82e3046ec6b27837301c5
titann07 <100381381+titann07@users.noreply.github.com>
2025-01-08 15:16:04 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_P0d2jjOgL', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '26vgbq7ebinqlo3a311gun3cof',// e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        ClientSecret: '10l97oo22vhl2cub0lvtbjtuqig9f2tr6ff6e7pbg61fkdhr5173'
+        ClientSecret: '10l97oo22vhl2cub0lvtbjtuqig9f2tr6ff6e7pbg61fkdhr5173',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
